491a3074d09520ba29cdf2356763eb295b0db81b
WilsonMachado <wilson.daniel12@gmail.com>
2023-05-28 22:46:06 -0500

    Se mejoran los estilos del modal. Falta agregar modal para los errores del formulario

--- docs/js/app.js
+++ docs/js/app.js
@@ -13,7 +13,6 @@ function submitForm(e){
     let message = document.getElementById("message").value;
     
     if (name === "" || email === "" || message === "") {
-        
         alert("Por favor, completa todos los campos del formulario.");
       
     } else {       
@@ -26,22 +25,24 @@ function showModal(nombre){
     
     let backgroudModal = document.createElement("div");
     backgroudModal.className = "modal-bg";
+    backgroudModal.id = "modal-bg";
 
     let modal = document.createElement("div");
     modal.className = "modal-content";
-    modal.id = "modal-content";
 
     document.getElementById("contact").appendChild(backgroudModal);
     
     backgroudModal.appendChild(modal);
     modal.insertAdjacentHTML("beforeend", `<h3>Thanks for your message, ${nombre}!</h3>`);
     modal.insertAdjacentHTML("beforeend", "<p>I have received your message. I will contact you as soon as possible.</p>");
-    modal.insertAdjacentHTML("beforeend", "<div class='contact-button'><a href='javascript:hideModal()'>Okay!</a></div>");
+    modal.insertAdjacentHTML("beforeend", "<div class='contact-button button-modal'><a href='javascript:hideModal()'>Okay!</a></div>");
 
 
     document.getElementById("body").classList.add("no-scroll");
 }
 
 function hideModal(){
-    console.log("Listo, mi papá! Cerrando el modal");
+    let modal = document.getElementById("modal-bg");
+    document.getElementById("contact").removeChild(modal);
+    document.getElementById("body").classList.remove("no-scroll");
 }
